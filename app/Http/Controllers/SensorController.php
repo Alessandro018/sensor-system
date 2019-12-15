2193eb33768f818136dee6eba7b8b236adf512f0
Alessandro018 <alessandrosilva325@gmail.com>
2019-12-15 18:03:48 -0300

    Atualização do sensor

--- app/Http/Controllers/SensorController.php
+++ app/Http/Controllers/SensorController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Sensor;
-use App\API\ApiError;
 
 class SensorController extends Controller
 {
@@ -17,7 +16,7 @@ class SensorController extends Controller
     public function index()
     {
         $sensor = Sensor::all();
-        return response()->json($this->sensor->paginate(6));
+        return response()->json($this->sensor->paginate(6), 200);
     }
 
     public function store(Request $request)
@@ -27,5 +26,30 @@ class SensorController extends Controller
             'tipo' => 'required'
         ]);
         Sensor::create($request->all());
+        return response()->json(['mensagem' => 'Sensor cadastrado com sucesso'], 201);
+    }
+
+    public function show($id)
+    {
+    	$sensor = Sensor::find($id);
+        if(!$sensor) 
+        {
+            return response()->json(['error' => 'Sensor não encontrado'], 404);
+        }
+        $data = ['data' => $sensor];
+	    return response()->json($data);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $sensor = $this->sensor->find($id);
+        if(!$sensor) 
+        {
+            return response()->json(['error' => 'Sensor não encontrado'], 404);
+        }
+        $sensor->update($request->all());
+
+        $return = ['data' => ['mensagem' => 'Produto atualizado com sucesso!']];
+        return response()->json($return, 200);
     }
 }
