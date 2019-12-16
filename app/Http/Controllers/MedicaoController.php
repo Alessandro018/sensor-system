cfdbfc303389b2130d621d8b2480669665a4a517
gabrielpessoa <gabrielpessoanascimento@gmail.com>
2019-12-15 22:04:52 -0300

    checkar se o sensor existe

--- app/Http/Controllers/MedicaoController.php
+++ app/Http/Controllers/MedicaoController.php
@@ -23,52 +23,48 @@ class MedicaoController extends Controller
     public function store(Request $request)
     {
         $sensor = Sensor::find($request->sensor_id);
-        if($sensor->tipo == 'temperatura'){
-            $request->validate([
-                'sensor_id' => 'required|integer',
-                'valor' => 'required|integer|min:-100|max:100',
-                'data_horario' => 'required|date',
-            ]);
+        if(!empty($sensor)){
+            if($sensor->tipo == 'temperatura'){
+                $request->validate([
+                    'sensor_id' => 'required|integer',
+                    'valor' => 'required|integer|min:-100|max:100',
+                    'data_horario' => 'required|date',
+                ]);
 
-            Medicao::create($request->all());
-            return response()->json(['mensagem' => 'Medicao cadastrado com sucesso'], 201);
-        }elseif($sensor->tipo == 'luminosidade'){
-            $request->validate([
-                'sensor_id' => 'required|integer',
-                'valor' => 'required|integer|min:0|max:100',
-                'data_horario' => 'required|date',
-            ]);
+                Medicao::create($request->all());
+                return response()->json(['mensagem' => 'Medicao cadastrado com sucesso'], 201);
+            }elseif($sensor->tipo == 'luminosidade'){
+                $request->validate([
+                    'sensor_id' => 'required|integer',
+                    'valor' => 'required|integer|min:0|max:100',
+                    'data_horario' => 'required|date',
+                ]);
 
-            Medicao::create($request->all());
-            return response()->json(['mensagem' => 'Medicao cadastrado com sucesso'], 201);
-        }elseif($sensor->tipo == 'presença'){
-            $request->validate([
-                'sensor_id' => 'required|integer',
-                'valor' => 'required|integer|min:0|max:1',
-                'data_horario' => 'required|date',
-            ]);
+                Medicao::create($request->all());
+                return response()->json(['mensagem' => 'Medicao cadastrado com sucesso'], 201);
+            }elseif($sensor->tipo == 'presença'){
+                $request->validate([
+                    'sensor_id' => 'required|integer',
+                    'valor' => 'required|integer|min:0|max:1',
+                    'data_horario' => 'required|date',
+                ]);
 
-            Medicao::create($request->all());
-            return response()->json(['mensagem' => 'Medicao cadastrado com sucesso'], 201);
-        }elseif($sensor->tipo == 'magnético'){
-            $request->validate([
-                'sensor_id' => 'required|integer',
-                'valor' => 'required|integer|min:0|max:1',
-                'data_horario' => 'required|date',
-            ]);
+                Medicao::create($request->all());
+                return response()->json(['mensagem' => 'Medicao cadastrado com sucesso'], 201);
+            }elseif($sensor->tipo == 'magnético'){
+                $request->validate([
+                    'sensor_id' => 'required|integer',
+                    'valor' => 'required|integer|min:0|max:1',
+                    'data_horario' => 'required|date',
+                ]);
 
-            Medicao::create($request->all());
-            return response()->json(['mensagem' => 'Medicao cadastrado com sucesso'], 201);
+                Medicao::create($request->all());
+                return response()->json(['mensagem' => 'Medicao cadastrado com sucesso'], 201);
+            }
+                
         }else{
-            $request->validate([
-                'sensor_id' => 'required|integer',
-                'valor' => 'required|integer',
-                'data_horario' => 'required|date',
-            ]);
-
             return response()->json(['data' => ['error' => 'sensor não encontrado']], 404);
         }
-
 
     }
 
